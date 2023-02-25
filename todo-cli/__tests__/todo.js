c3eb813eae5a6250eee572cda5e867f37140a9a7
Test User <20110089@hicet.ac.in>
2023-02-25 17:36:58 +0530

    Commit

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,27 @@
+const todoList = require('../todo');
+const {all,markAsComplete,add} = todoList();
+describe("Todolist test suite",()=>{
+    beforeAll(()=>{
+        add({
+            title: "Test Todo",
+            completed: false,
+            dueDate: new Date().toLocaleDateString("en-CA")
+        })
+    })
+    test("should add new todo",()=>{
+        const todoItemsCount = all.length;
+        add(
+            {
+                title: "Test Todo",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA")
+        }
+        );
+        expect(all.length).toBe(todoItemsCount+1) ;
+    })
+    test("should mark as complete",()=>{
+        expect(all[0].completed).toBe(false);
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true);
+    })
+})
