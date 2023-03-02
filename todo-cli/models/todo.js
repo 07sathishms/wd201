e455e055cbacee8bd003db4c00e0645740c77ae9
Test User <20110089@hicet.ac.in>
2023-03-02 19:19:33 +0530

    Commit

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model, Op
+  Model, Op, DATE
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
@@ -87,9 +87,15 @@ module.exports = (sequelize, DataTypes) => {
     displayableString() {
       let check = this.completed ? "[x]" : "[ ]";
       const day = new Date(this.dueDate);
-      return day.getDate() === new Date().getDate()
-      ? `${this.id}. ${check} ${this.title}`.trim()
-      : `${this.id}. ${check} ${this.title} ${this.dueDate}`.trim()
+     if(day.getDate() === new Date().getDate()){
+       return`${this.id}. ${check} ${this.title} ${this.dueDate}`.trim()
+     }else if(day < new Date()){
+      return`${this.id}. ${check} ${this.title}`
+     }
+     else{
+      return`${this.id}. ${check} ${this.title} ${this.dueDate}`.trim()
+
+     }
      
     }
   }
