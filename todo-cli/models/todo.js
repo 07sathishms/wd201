aea3d15f00c23d7f35519990d104321f40b9eaa1
Test User <20110089@hicet.ac.in>
2023-03-02 18:21:15 +0530

    Commit

---
+++ todo-cli/models/todo.js
@@ -0,0 +1,101 @@
+'use strict';
+const {
+  Model, Op
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static async addTask(params) {
+      return await Todo.create(params);
+    }
+    static async showList() {
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      // FILL IN HERE
+     const ov = await Todo.overdue();
+     ov.forEach(todo => console.log(todo.displayableString()));
+    
+    
+      console.log("\n");
+
+      console.log("Due Today");
+      // FILL IN HERE
+      const td = await Todo.overdue();
+     td.forEach(todo => console.log(todo.displayableString()));
+      console.log("\n");
+
+      console.log("Due Later");
+      // FILL IN HERE
+      const dl = await Todo.overdue();
+     dl.forEach(todo => console.log(todo.displayableString()));
+    }
+
+    static async overdue() {
+      // FILL IN HERE TO RETURN OVERDUE ITEMS
+      const date = new Date();
+      return await Todo.findAll({
+        where: {
+          dueDate: {[Op.lt]:date},
+          completed: false
+        },
+        order: [['dueDate','ASC']]
+
+      });
+    }
+
+    static async dueToday() {
+      // FILL IN HERE TO RETURN ITEMS DUE tODAY
+      const date = new Date();
+      return await Todo.findAll({
+        where: {
+          dueDate: {[Op.eq]:date},
+          completed: false
+        },
+        order: [['dueDate','ASC']]
+
+      });
+    }
+
+    static async dueLater() {
+      // FILL IN HERE TO RETURN ITEMS DUE LATER
+      const date = new Date();
+      return await Todo.findAll({
+        where: {
+          dueDate: {[Op.gt]:date},
+          completed: false
+        },
+        order: [['dueDate','ASC']]
+
+      });
+    }
+
+    static async markAsComplete(id) {
+      // FILL IN HERE TO MARK AN ITEM AS COMPLETE
+      const todo = await Todo.findByPk(id);
+      if(todo){
+        todo.completed = true;
+        await todo.save();
+      }     
+
+    }
+
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+    }
+  }
+  Todo.init({
+    title: DataTypes.STRING,
+    dueDate: DataTypes.DATEONLY,
+    completed: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Todo',
+  });
+  return Todo;
+};
