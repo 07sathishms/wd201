3bb4e0b2defa93895109bda06bb21d7758f18013
Test User <20110089@hicet.ac.in>
2023-03-02 19:10:45 +0530

    Commit

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -87,6 +87,10 @@ module.exports = (sequelize, DataTypes) => {
     displayableString() {
       let check = this.completed ? "[x]" : "[ ]";
       const day = new Date(this.dueDate);
+     if(this.completed === true){
+      return `${this.id}. ${check} ${this.title} ${this.dueDate}`.trim()
+
+      }
       return day.getDate() === new Date().getDate()
       ? `${this.id}. ${check} ${this.title}`.trim()
       : `${this.id}. ${check} ${this.title} ${this.dueDate}`.trim()
